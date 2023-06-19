22fc954929f1304c3edbc65072ac9c365783bd44
marcin9292 <marcin.skrzyniarz92@gmail.com>
2023-06-19 21:33:44 +0200

    Poprawnienie kodu

--- js/script.js
+++ js/script.js
@@ -2,69 +2,33 @@ function playAudio(url) {
     new Audio(url).play();
 }
 
-function playW(){
-    playAudio('sound/W.mp3');
-}
-
-function playS(){
-    playAudio('sound/S.mp3');
-}
-
-function playD(){
-    playAudio('sound/D.mp3');
-}
-
-function playF(){
-    playAudio('sound/F.mp3');
-}
-
-function playG(){
-    playAudio('sound/G.mp3');
-}
-
-function playH(){
-    playAudio('sound/H.mp3');
-}
-
-function playJ(){
-    playAudio('sound/J.mp3');
-}
-
-function playK(){
-    playAudio('sound/K.mp3');
-}
-
-function playL(){
-    playAudio('sound/L.mp3');
-}
-
 document.addEventListener('keypress', (event) => {
     var name = event.key;
     if(name === 'w' || name === 'W'){
-        playW();
+        playAudio('sound/W.mp3');
     }
     if(name === 's' || name === 'S'){
-        playS();
+        playAudio('sound/S.mp3');
     }
     if(name === 'd' || name === 'D'){
-        playD();
+        playAudio('sound/D.mp3');
     }
     if(name === 'f' || name === 'F'){
-        playF();
+        playAudio('sound/F.mp3');
     }
     if(name === 'g' || name === 'G'){
-        playG();
+        playAudio('sound/G.mp3');
     }
     if(name === 'h' || name === 'H'){
-        playH();
+        playAudio('sound/H.mp3');
     }
     if(name === 'j' || name === 'J'){
-        playJ();
+        playAudio('sound/J.mp3');
     }
     if(name === 'k' || name === 'K'){
-        playK();
+        playAudio('sound/K.mp3');
     }
     if(name === 'l' || name === 'L'){
-        playL();
+        playAudio('sound/L.mp3');
     }
   }, false);
